f07fb37672041566c5f9e7d705ca7e206f9bb285
Wojciech <wksosnowski@gmail.com>
2025-10-07 14:08:11 +0200

    add theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'mbbill/undotree',
+  keys = {
+    { '<leader>u', ':UndotreeToggle<CR>', desc = 'Toggle Undo Tree' },
+  },
+  {
+    'sainnhe/everforest',
+    lazy = false, -- load on startup
+    priority = 1000, -- make sure it's loaded before other plugins
+    config = function()
+      vim.g.everforest_background = 'hard' -- soft, medium, hard
+      vim.g.everforest_enable_italic = 1
+      vim.g.everforest_diagnostic_text_highlight = 1
+      vim.g.everforest_better_performance = 1
+      vim.cmd.colorscheme 'everforest'
+    end,
+  },
+}
